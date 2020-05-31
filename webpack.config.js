fb7da2b19ec1364a4bf3380d33edb5ab7594e682
mcrramirez <mcr_ramirez@yahoo.com>
2020-05-31 07:36:38 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  context: path.join(__dirname, "src"),
+  entry: ["./main.js"],
+  output: {
+    path: path.join(__dirname, "www"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".jsx", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      }
+    ]
+  }
+};
